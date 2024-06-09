04ad1aabc8bf2b94b47c1b2dc5999a819b662d56
vocaltech <it@vocaltech.fr>
2024-06-09 19:18:13 +0200

    Add string test in givenLatLg_whenNewPos_thenOk()

--- src/test/java/fr/vocaltech/jenkins/MainTest.java
+++ src/test/java/fr/vocaltech/jenkins/MainTest.java
@@ -11,5 +11,6 @@ class MainTest {
         var position = new Position(45.5, 1.4);
         assertEquals(45.5, position.latitude());
         assertEquals(1.4, position.longitude());
+        assertTrue(position.toString().startsWith("Position"));
     }
 }
